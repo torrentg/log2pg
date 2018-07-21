9baaea0fd03e7ec2767026c719450cb0d8eebe5a
Gerard Torrent <gerard@generacio.com>
2018-07-21 08:32:39 +0200

    struct string moved to stringbuf (changes in unit test).

--- test/stringbuf_test.c
+++ test/stringbuf_test.c
@@ -2,82 +2,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "string.h"
+#include "stringbuf.h"
 
 /*
- * gcc -iquote ../src -o string_test string_test.c ../src/string.c
+ * gcc -iquote ../src -o stringbuf_test stringbuf_test.c ../src/stringbuf.c
  */
 
 void test1()
 {
-  string_t str = {0};
+  stringbuf_t str = {0};
   
   printf("TEST1 --------------------\n");
 
-  string_append(&str, "hola ");
+  stringbuf_append(&str, "hola ");
   printf("str=%s, len=%zu, capacity=%zu, strlen=%zu\n", str.data, str.length, str.capacity, strlen(str.data));
   
-  string_append(&str, "don ");
+  stringbuf_append(&str, "don ");
   printf("str=%s, len=%zu, capacity=%zu, strlen=%zu\n", str.data, str.length, str.capacity, strlen(str.data));
 
-  string_append(&str, "pepito, hola don ");
+  stringbuf_append(&str, "pepito, hola don ");
   printf("str=%s, len=%zu, capacity=%zu, strlen=%zu\n", str.data, str.length, str.capacity, strlen(str.data));
 
-  string_append(&str, "jose, pepito ");
+  stringbuf_append(&str, "jose, pepito ");
   printf("str=%s, len=%zu, capacity=%zu, strlen=%zu\n", str.data, str.length, str.capacity, strlen(str.data));
 
-  string_append(&str, "lomo");
+  stringbuf_append(&str, "lomo");
   printf("str=%s, len=%zu, capacity=%zu, strlen=%zu\n", str.data, str.length, str.capacity, strlen(str.data));
 
-  string_replace(&str, "pepito", "jose");  
+  stringbuf_replace(&str, "pepito", "jose");  
   printf("str=%s, len=%zu, capacity=%zu, strlen=%zu\n", str.data, str.length, str.capacity, strlen(str.data));
   
-  string_reset(&str);
+  stringbuf_reset(&str);
 }
 
 void test2()
 {
-  string_t str = {0};
+  stringbuf_t str = {0};
 
   printf("TEST2 --------------------\n");
   
-  string_append(&str, "hola don jose, hola don pepito, jose lomo");
+  stringbuf_append(&str, "hola don jose, hola don pepito, jose lomo");
   printf("str=%s, len=%zu, capacity=%zu, strlen=%zu\n", str.data, str.length, str.capacity, strlen(str.data));
 
-  string_replace(&str, "jose", "pepito");  
+  stringbuf_replace(&str, "jose", "pepito");  
   printf("str=%s, len=%zu, capacity=%zu, strlen=%zu\n", str.data, str.length, str.capacity, strlen(str.data));
   
-  string_reset(&str);
+  stringbuf_reset(&str);
 }
 
 void test3()
 {
-  string_t str = {0};
+  stringbuf_t str = {0};
 
   printf("TEST3 --------------------\n");
   
-  string_append(&str, "hola don xxx, hola don xxx, xxx lomo");
+  stringbuf_append(&str, "hola don xxx, hola don xxx, xxx lomo");
   printf("str=%s, len=%zu, capacity=%zu, strlen=%zu\n", str.data, str.length, str.capacity, strlen(str.data));
 
-  string_replace(&str, "xxx", "yyy");  
+  stringbuf_replace(&str, "xxx", "yyy");  
   printf("str=%s, len=%zu, capacity=%zu, strlen=%zu\n", str.data, str.length, str.capacity, strlen(str.data));
   
-  string_reset(&str);
+  stringbuf_reset(&str);
 }
 
 void test4()
 {
-  string_t str = {0};
+  stringbuf_t str = {0};
 
   printf("TEST4 --------------------\n");
   
-  string_append(&str, "hola don xxx, hola don xxx, xxx lomo");
+  stringbuf_append(&str, "hola don xxx, hola don xxx, xxx lomo");
   printf("str=%s, len=%zu, capacity=%zu, strlen=%zu\n", str.data, str.length, str.capacity, strlen(str.data));
 
-  string_replace(&str, "xxx", NULL);  
+  stringbuf_replace(&str, "xxx", NULL);  
   printf("str=%s, len=%zu, capacity=%zu, strlen=%zu\n", str.data, str.length, str.capacity, strlen(str.data));
   
-  string_reset(&str);
+  stringbuf_reset(&str);
 }
 
 // main function
